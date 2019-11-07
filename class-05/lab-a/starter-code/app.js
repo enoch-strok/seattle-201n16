3e5d12ccbc691ea572ebeb9e7b175d78bd537945
Enoch Strok <50689197+enoch-strok@users.noreply.github.com>
2019-11-06 20:57:07 -0800

    Problem 01 (#2)
    * completed problem 01, ready for push and then merge and then switch to updated master and then pull new master to local and then create new branch for next problem
    
    * I didnt save the previous app_js file so re ACPing again and merging again

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -31,8 +31,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
 }
+
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
