3d8a41f21a6f507a437380e3937de9418838bd2d
Enoch Strok <50689197+enoch-strok@users.noreply.github.com>
2019-11-06 20:42:12 -0800

    completed problem 01, ready for push and then merge and then switch to updated master and then pull new master to local and then create new branch for next problem (#1)
    Merging with my own repo for problem 01

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -9,8 +9,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
-}
+    var sumAB = a + b;
+    var message = 'The sum of ' + a + ' and ' + b + ' is ' + sumAB;
+    var array01 = [sumAB, message];
+    console.log(array01);
+}   
+sum(4,7);
 
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
