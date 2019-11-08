fa4e5fd7e799f89138b73615303e93e4a2aa1eee
enoch-strok <enochstrok@outlook.com>
2019-11-07 19:26:01 -0800

    adding changes to problem_02 branch because i didnt update it propely last time because I didnt save changes before commiting and pushing it up to github

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -31,11 +31,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-}
+    var productAB = a * b;
+    var message = 'The product of ' + a + ' and ' + b + ' is ' + productAB + '.';
+    var array02 = [productAB, message];
+    console.log(array02);
+    return array02;
+}   
+multiply(5,9);
+
 
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
