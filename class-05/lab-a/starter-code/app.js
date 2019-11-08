aa5925b409c639223c9b7851f01f806d2678f9e3
enoch-strok <enochstrok@outlook.com>
2019-11-07 20:04:25 -0800

    I passed the sumAndMultiply problem 3, uploading completed branch

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -57,11 +57,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    var sumABC = a + b + c;
+    var productABC = a * b * c;
+    var sumText = a + ' and ' + b + ' and ' + c + ' sum to ' + sumABC + '.';
+    var productText = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productABC + '.';
+    var arrayABC = [sumABC, productABC, sumText, productText]
+    console.log(arrayABC);
+    return arrayABC;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
